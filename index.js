1c806c653ce95e721ed07b53016aae41a9f58be7
zw <1085383440@qq.com>
2019-08-27 00:08:37 +0800

    first step

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: './chrome-linux/chrome'
+  });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://www.nike.com/cn/launch/t/daybreak-lavender-mist/');
   await page.screenshot({path: 'example.png'});
 
   await browser.close();
